e4a52231d95cb7cf4471ca21a5739204ec590adb
PaoloSalvatico <paolosalvatico@icloud.com>
2021-11-14 17:17:36 +0100

    add spacing

--- Assets/VdM/Scripts/CubeCreation.cs
+++ Assets/VdM/Scripts/CubeCreation.cs
@@ -7,14 +7,14 @@ public class CubeCreation : MonoBehaviour
     public int size;
     public float spacing = 0.8f;
     public GameObject prefab;
-    Quaternion rot;
+    private Quaternion rot;
 
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i <= size; i++)
         {
-            Instantiate(prefab, new Vector3(i, 0, 0), rot);
+            Instantiate(prefab, new Vector3(i + spacing, 0, 0), rot);
         }
     }
 
